32bfcf60f61cc922eb2f84ed5a8c15439ae241c5
Andreas Seeger <splash@appfros.ch>
2023-12-14 16:15:22 +0100

    Initial Commit

---
+++ so-tcaThreePanelObservationBeta/so_tcaThreePanelObservationBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  so_tcaThreePanelObservationBetaApp.swift
+//  so-tcaThreePanelObservationBeta
+//
+//  Created by Andreas Seeger on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct so_tcaThreePanelObservationBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
